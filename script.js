c4ce8443bd7cce479d33d3213f5fcc9ecd7f628f
Sarah Bridgeman <skbridgeman@hotmail.com>
2022-11-25 13:11:39 -0500

    Add random computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+function getRandomInt(max) {
+    return Math.floor(Math.random * max)
+}
+let num = getRandomInt(3);
+
+function getComputerChoice(num) {
+    if (num == 0) {
+        console.log("Rock");
+    } else if (num == 1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
+
+getComputerChoice(2);
