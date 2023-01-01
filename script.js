19994bd837c2066808f413cdd3b9081af4a8ba13
Sarah Bridgeman <skbridgeman@hotmail.com>
2023-01-01 11:02:36 -0500

    Add event listeners and DOM methocs

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let countP1 = 0;
+let countPlayer = 0;
 let countCPU = 0;
+let playerSelection;
 
-function playRound() {
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
+let cpuplays = document.querySelector('.cpuplays');
+let round = document.querySelector('.round');
+let results = document.querySelector('.results');
+let playercount = document.querySelector('.playercount');
+let cpucount = document.querySelector('.cpucount');
+
+rock.addEventListener('click', () => {
+    playRound("rock");
+    });
+
+paper.addEventListener('click', () => {
+    playRound("paper");
+    });
+
+scissors.addEventListener('click', () => {
+    playRound("scissors");
+    });
+
+function playRound(playerSelection) {
     function getRandomInt() {
         return Math.floor(Math.random() * 3);
     }
-
     const num = getRandomInt();
 
     function getComputerChoice() {
@@ -17,64 +38,53 @@ function playRound() {
     } else if (num == 2) {
         return ("scissors");
     } else {
-        console.log("Something went wrong")
+        round.textContent = "Something went wrong";
     }
-}   
-
-
-    let playerSelection = prompt("What is your choice? Rock Paper Scissors");
+    } 
 
     const computerSelection = getComputerChoice();
-
-    console.log("CPU plays " + getComputerChoice());
-
+    cpuplays.textContent = "CPU plays " + getComputerChoice();
+    
     if (playerSelection == computerSelection) {
-        console.log("It's a tie. Play again.");
+        round.textContent = "It's a tie. Play again.";
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose. Paper covers rock.");
+        round.textContent = "You lose. Paper covers rock.";
         console.count("CPU");
-        countCPU++;
+        cpucount.textContent = "CPU: " + ++countCPU;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You win! Rock crushes scissors.");
+        round.textContent = "You win! Rock crushes scissors.";
         console.count("You");
-        countP1++;
+        playercount.textContent = "You: " + ++countPlayer;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You win! Paper covers rock.")
+        round.textContent = "You win! Paper covers rock.";
         console.count("You");
-        countP1++;
+        playercount.textContent = "You: " + ++countPlayer;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You lose. Scissors cuts paper.")
+        round.textContent = "You lose. Scissors cuts paper.";
         console.count("CPU");
-        countCPU++;
+        cpucount.textContent = "CPU: " + ++countCPU;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You lose. Rock crushes scissors.")
+        round.textContent = "You lose. Rock crushes scissors."
         console.count("CPU");
-        countCPU++;
+        cpucount.textContent = "CPU: " + ++countCPU;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You win! Scissors cuts paper.")
+        round.textContent = "You win! Scissors cuts paper.";
         console.count("You");
-        countP1++;
+        playercount.textContent = "You: " + ++countPlayer;
     } else {   
-        console.log("Choose a weapon.");
+        round.textContent = "Choose a weapon.";
     }
+
+    function winOrLose() {    
+        if (countPlayer === 5) {  
+            results.textContent = "YOU ARE THE WINNER!";
+        } else if (countCPU === 5) {
+            results.textContent = "YOU ARE THE LOSER.";
+        } 
+        }
+    winOrLose();
+       
 }
-
-function game() {
-    while (countP1 < 3 && countCPU < 3) {
-        playRound();
-    } 
-}
-
-function winOrLose() {    
-    if (countP1 == 3) {  
-        console.log("YOU ARE THE WINNER!");
-    } else if (countCPU == 3) {
-        console.log("YOU ARE THE LOSER.");
-    } else { 
-        playRound();
-    }
-}
-
-game();
-
-winOrLose();
+    
+            
+ 
